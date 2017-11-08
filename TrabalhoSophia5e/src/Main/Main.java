212cf536c509d6b7e46462ebeda039b6b000068d
gabrielsox <gabrielgabriel100@hotmail.com>
2017-11-08 17:21:33 -0200

    ALTERAÇÕES DE LINHA E ESPAÇO

--- TrabalhoSophia5e/src/Main/Main.java
+++ TrabalhoSophia5e/src/Main/Main.java
@@ -2,17 +2,13 @@ package Main;
 
 import Banco.Cliente;
 import Banco.Conta;
-import Banco.Extrato;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
+
 
 public class Main {
 
     public static void main(String[] args) {
 
-        Cliente cliente1 = new Cliente("10790319608", "Igor", "Araujo");
-
+        Cliente cliente1 = new Cliente("10780319708", "Igor", "Araujo");
         Conta c = new Conta(cliente1, 0, 0);
         c.iniciar();
 
